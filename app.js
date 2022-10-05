76004119fed454d68e4f6bd312d9c172dc95a096
Alexander <lenchenkov.a.d@gmail.com>
2022-10-05 10:42:34 +0500

    Создание сервера

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const fs = require('fs');
-const zlib = require('zlib');
+const http = require('http');
+const { json } = require('stream/consumers');
 
-const readStream = fs.createReadStream('./docs/text.txt');
-const writeStream = fs.createWriteStream('./docs/new-text.txt');
-const compressStream = zlib.createGzip();
+const PORT = 3000;
 
-// readStream.on('data', chunk => {
-// 	writeStream.write('-------------------------------------------');
-// 	writeStream.write(chunk);
-// });
-// Функция для остановки потока при ошибке
-const handleError = () => {
-	console.log('Error');
-	readStream.destroy();
-	writeStream.end('Finished with error...');
-};
-readStream.on('error', handleError).pipe(compressStream).pipe(writeStream).on('error', handleError);
+const server = http.createServer((req, res) => {
+	console.log('Server request');
+	console.log(req.url, req.method);
+
+	res.setHeader('Context-Type', 'application/json');
+	// res.write(`<h1>Hello!</h1>`);
+	// res.write(`<p>Hello. It is description!</p>`);
+
+	const data = JSON.stringify([
+		{ name: 'Alex', age: 19 },
+		{ name: 'Daniil', age: 18 },
+	]);
+
+	res.end(data);
+});
+
+server.listen(PORT, 'localhost', error => {
+	error ? console.log(error) : console.log(`Listening port ${PORT}`);
+});
